a9aa309a54c46d376cc97391dc467089a7b3d948
sixway <lw.2675@qq.com>
2022-09-19 09:54:34 +0800

    fix: add parameter validation

--- logic/handler_test.go
+++ logic/handler_test.go
@@ -57,6 +57,23 @@ func TestShareAPIOk(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "0", w.Body.String())
 }
+func TestShareAPIParamError(t *testing.T) {
+	w, c, r := httpTestHelper()
+	// following request missing param content
+	c.Request, _ = http.NewRequest("POST", "/share", nil)
+	c.Request.PostForm = url.Values{
+		//"content": []string{""},
+	}
+	storage := &MockStorage{
+		mapping:    map[string]string{},
+		raiseError: false,
+		key:        0,
+	}
+	r.POST("/share", ShareAPI(storage))
+	r.ServeHTTP(w, c.Request)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "require parameter content", w.Body.String())
+}
 func TestShareAPIFail(t *testing.T) {
 	w, c, r := httpTestHelper()
 	c.Request, _ = http.NewRequest("POST", "/share", nil)
@@ -106,6 +123,23 @@ func TestQueryAPIFail(t *testing.T) {
 	r.ServeHTTP(w, c.Request)
 	assert.Equal(t, http.StatusNotFound, w.Code)
 	assert.Equal(t, "not found", w.Body.String())
+}
+
+func TestQueryAPIParamError(t *testing.T) {
+	storage := &MockStorage{
+		mapping:    map[string]string{},
+		raiseError: false,
+		key:        0,
+	}
+	testVal := "a quick fox jumps over a lazy dog"
+	key, _ := storage.Put(context.Background(), testVal, time.Second)
+	w, c, r := httpTestHelper()
+	// following request do not contain the 'tid' param
+	c.Request, _ = http.NewRequest("GET", fmt.Sprintf("/query?x=%s", key), nil)
+	r.GET("/query", QueryAPI(storage))
+	r.ServeHTTP(w, c.Request)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "require parameter tid", w.Body.String())
 }
 
 // httpTestHelper 返回用于测试的三个http相关对象
